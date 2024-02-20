6e6bb600075f13c3d1390b8ff7870d9e957474da
Katuki1 <98540297+Katuki1@users.noreply.github.com>
2024-02-20 07:21:30 +0300

    Arrays

---
+++ arrays/src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        String[] names = {"John", "Smith"};
+
+        String[] names;
+        names = new String[5];
+        names[0] = "John";
+        names[1] = "Smith";
+        names[2] = "Willy";
+        names[3] = "Wonka";
+
+        Main main = new Main();
+        main.displayNames(names);
+        System.out.println("new value: "+ names[1]);
+
+        //to pass each item in array- for each loop
+//        for(String name: names) {
+//            System.out.println(name);
+//        }
+
+    }
+
+    //call by reference
+    void displayNames (String[] names){
+        System.out.println("old value: " +names[1]);
+        names[1] = "Luka";
+    }
+}
